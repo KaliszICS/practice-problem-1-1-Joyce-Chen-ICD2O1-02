66f1081d411f69d094bc7fc34aa1552cf685f949
Joyce-Chen-ICD2O1-02 <joyce.chen@student.tdsb.on.ca>
2024-09-13 18:14:26 +0000

    added answers

--- PP1_1.py
+++ PP1_1.py
@@ -1,15 +1,29 @@
+'''
+Lesson 1.1 - Output
+Author: Joyce Chen
+Date Created: September 12, 2024
+Date Last Modified: September 12, 2024
+'''
 
 def q1():
+        print("hello world")
   #Write Assignment code here
   #Remember your code must be indented once
 
 def q2():
+      print(1)
+      print(2)
+      print(3)
+      print(4)
+      print(5)
   #Write Assignment code here
   
 def q3():
+       print(" Help Me!")
   #Write Assignment code here
   
 def q4():
+      print("3 x 3 = 9") 
   #Write Assignment code here
 
 #Do not alter anything after this line
